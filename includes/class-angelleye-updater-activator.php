943adad3acaaf872aae0d52aa99b3268ace80d1b
mbj-webdevelopment <mbjwebdevelopment@gmail.com>
2015-09-07 14:46:13 +0530

    remove PayPal IPN for WordPress plugin required condition, ref #22

--- includes/class-angelleye-updater-activator.php
+++ includes/class-angelleye-updater-activator.php
@@ -16,19 +16,13 @@ class AngellEYE_Updater_Activator {
      * @since    1.0.0
      */
     public static function activate() {
-        if (!defined('PIW_PLUGIN_BASENAME')) {
-            deactivate_plugins(AU_PLUGIN_DIR . '/angelleye-updater.php');
-            wp_die("<strong>Angell EYE Updater</strong> requires <strong>PayPal IPN for WordPress</strong> plugin to work normally. Please activate it or install it.<br /><br />Back to the WordPress <a href='" . get_admin_url(null, 'plugins.php') . "'>Plugins page</a>.");
-        } else {
-
-            /**
-             * Log activation in Angell EYE database via web service.
-             */
-            $log_url = $_SERVER['HTTP_HOST'];
-            $log_plugin_id = 12;
-            $log_activation_status = 1;
-            wp_remote_request('https://www.angelleye.com/web-services/wordpress/update-plugin-status.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
-        }
+        /**
+         * Log activation in Angell EYE database via web service.
+         */
+        $log_url = $_SERVER['HTTP_HOST'];
+        $log_plugin_id = 12;
+        $log_activation_status = 1;
+        wp_remote_request('https://www.angelleye.com/web-services/wordpress/update-plugin-status.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
     }
 
 }

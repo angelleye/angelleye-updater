822f06a07446a3329c2fffdbd665a923ee4e6cb5
Andrew Angell <andrew@angelleye.com>
2016-09-15 04:45:40 -0500

    Removes remote logging, ref #23

--- includes/class-angelleye-updater-activator.php
+++ includes/class-angelleye-updater-activator.php
@@ -16,13 +16,7 @@ class AngellEYE_Updater_Activator {
      * @since    1.0.0
      */
     public static function activate() {
-        /**
-         * Log activation in Angell EYE database via web service.
-         */
-        $log_url = $_SERVER['HTTP_HOST'];
-        $log_plugin_id = 12;
-        $log_activation_status = 1;
-        wp_remote_request('https://www.angelleye.com/web-services/wordpress/update-plugin-status.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
+
     }
 
 }

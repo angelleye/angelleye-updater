99082c93879d7c48f4ca0590687856b6bf3f2dab
angelleye <andrew@angelleye.com>
2018-06-14 20:49:40 -0500

    AEUP-34 - Updates verbiage

--- screens/screen-help.php
+++ screens/screen-help.php
@@ -1,6 +1,6 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly   ?>
 <div id="col-container" class="about-wrap">
     <div class="col-wrap">
-        <?php echo '<h2>' . __('Help is coming soon', 'angelleye-updater') . '</h2>'; ?>
+        <?php echo '<h2>' . __('Documentation as well as Angell EYE news and updates will be available here soon!', 'angelleye-updater') . '</h2>'; ?>
     </div>
 </div>    
